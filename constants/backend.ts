7ea0c4456e4a97c5dde861cce28f909cf833d5d1
Afshin Mokhtari <afshinator@gmail.com>
2025-11-20 11:13:49 -0800

    change name of app

--- constants/backend.ts
+++ constants/backend.ts
@@ -1,4 +1,4 @@
 // constants/backend.ts
 
 export const BACKEND_BASE_URL = 'https://cryptospect-backend.vercel.app';
-export const BACKEND_API_KEY = 'crypto_spect_2024_abc123xyz789';
+export const BACKEND_API_KEY = 'crypto_spect_2024_abc123xyz789';  // ⚠️ TODO change this :-)  
